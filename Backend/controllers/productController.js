cfc2dc5cbcc396a6f9c355a327c18ad6a908b317
Swikar Adhikari <swikara082@gmail.com>
2021-11-20 09:29:24 +0545

    Added The Functionality To Update And Delete The Product Through Admin Routes

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 
-// Creating new product that will go the url /api/v1/product/new
+// Creating new product that will go the url /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
 
@@ -35,5 +35,48 @@ exports.getSingleProduct = async (req, res, next) => {
   res.status(200).json({
     sucess: true,
     product,
+  });
+};
+
+// Update Product Details to the url => /api/v1/admin/product/:id
+
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      sucess: false,
+      error: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    sucess: true,
+    product,
+  });
+};
+
+// Delete Product Details to the url => /api/v1/admin/product/:id
+
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      sucess: false,
+      error: "Product not found",
+    });
+  }
+
+  await product.deleteOne();
+
+  res.status(200).json({
+    sucess: true,
+    message: "Product Deleted",
   });
 };
