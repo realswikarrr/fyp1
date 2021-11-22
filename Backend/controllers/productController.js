cb3d376d3200f4a83cb51f430382634931e41047
Swikar Adhikari <swikara082@gmail.com>
2021-11-22 09:33:03 +0545

    Added the functionality for filtering product using category and price , in the function appFeatures.js

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -15,7 +15,9 @@ exports.newProduct = catchAsyncError(async (req, res, next) => {
 
 // Creating a function to get all the products to the url => /api/v1/products
 exports.getProducts = catchAsyncError(async (req, res, next) => {
-  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter();
   const products = await apiFeatures.query;
 
   res.status(200).json({
--- Backend/utils/apiFeatures.js
+++ Backend/utils/apiFeatures.js
@@ -16,6 +16,21 @@ class APIFeatures {
     this.query = this.query.find({ ...keyword });
     return this;
   }
+
+  filter() {
+    const queryCopy = { ...this.queryString };
+
+    // Removing the fields from the query string
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((field) => delete queryCopy[field]);
+
+    // Advanced filtering (e.g. price, ratings)
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 }
 
 module.exports = APIFeatures;
